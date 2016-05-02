0a988b626a6961af33877a68a46a47129fa13f16
Andrew Feldman <abf149@mit.edu>
2016-05-01 22:16:07 -0400

    Added nonfunctional cmd args for full-plate printing, offset, and rescale.

--- cam.py
+++ cam.py
@@ -39,6 +39,9 @@ parser.add_argument('Plate X', type=float, help='X coordinate of the plates top-
 parser.add_argument('Plate Y', type=float, help='Y coordinate of the plates top-left corner')
 parser.add_argument('Plate Z', type=float, help='Z coordinate of the plate')
 parser.add_argument('-of', help='Name of the file to create for the tool\'s GCODE output. A .gcode extension is advisable. If omitted, the default output file has the same base filename and path as the input, but with a .gcode extension.', default="0|")
+parser.add_argument('-full_plate', action='store_true', help='Rescale the input image to the size of the plate. Cancels the effects of -offset and -resize')
+parser.add_argument('-offset', help='Provide an ordered pair x,y representing the offset of the image from the plate\'s top-right corner.', default="(0,0)")
+parser.add_argument('-rescale', type=float, help='Provide an ordered pair x,y representing the offset of the image from the plate\'s top-right corner.', default="1.0")
 
 input_vars = vars(parser.parse_args())
 plate_x=input_vars['Plate X']
@@ -47,12 +50,24 @@ plate_z=input_vars['Plate Z']
 plate_width=input_vars['Plate Width']
 plate_height=input_vars['Plate Height']
 input_filename=input_vars['Input File']
-output_filename=input_vars['of']
 
-#Determine output filename
+#Handle optional arguments
+output_filename=input_vars['of']
+full_plate=input_vars['full_plate']
+offset=input_vars['offset']
+rescale=input_vars['rescale']
+
 pre, ext = os.path.splitext(input_filename)
 if output_filename=="0|": output_filename=pre+".gcode"
 
+if offset=="(0,0)": offset=(0,0)
+else:
+    offset=tuple([float(i) for i in offset[1:-1].split(',')])
+    if len(offset) != 2: raise Exception('Invalid offset', '\'-offset\' must specify exactly two values.')
+
+
+
+    
 #Load configuration file
 config = ConfigParser.RawConfigParser()
 config.read('cam.cfg')
