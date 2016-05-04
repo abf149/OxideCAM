312f146e0a2ee538bc713f9f25c8ebdfd91fe94b
Andrew Feldman <abf149@mit.edu>
2016-05-03 23:29:34 -0400

    calibration_lookup.py is functional.

--- calibration_lookup.py
+++ calibration_lookup.py
@@ -18,14 +18,15 @@ import math
 
 class CalibrationLookupTable:
     #sig_figs: number of matching significant figures necessary to establish equality between floats
-    def __init__(decimal_places):
-        self.cali_fn=cali_fn
+    def __init__(self,decimal_places):
+        self.cali_fn=""
         self.equality_threshold=math.pow(10,-decimal_places)
         self.table=[]
         self.table_length=0
         
     #calibration_file_name: filename of the file containing OxidePrinter voltage source calibration data
-    def load_calibration(calibration_file_name):
+    def load_calibration(self,calibration_file_name):
+        self.cali_fn=calibration_file_name    
         self.table=[]
         self.table_length=0
         
@@ -42,18 +43,21 @@ class CalibrationLookupTable:
     #log complexity binary search for a table entry that matches output_voltage to within self.equality_threshold
     #Another module calls this method as search(output_voltage), leaving other arguments to their default values.
     #The other arguments are used for recursion.
-    def search(output_voltage,ptr=-1,L=-1,prev_v_pwm=(-1,-1)):
+    def search(self,output_voltage,ptr=-1,L=-1,prev_v_pwm=(-1,-1)):
+    
         #Default values
         if L==-1: L=self.table_length
         if ptr==-1: ptr=L/2
-    
+        
         if abs(self.table[ptr]-output_voltage)<self.equality_threshold: return ptr #Exact match
         
-        if L==1: #Terminating condition: choose PWM byte that best approximates output_voltage
-            return ptr + ((prev_v_pwm[0]<self.table[ptr]) != (abs(prev_v_pwm[0]-output_voltage)>abs(self.table[ptr]-output_voltage)))
+        #Terminating condition: the below logical operation effectively rounds the PWM byte to a value that must closely reproduces output_voltage
+        #It also ensures that the value returned is between 0 and 255
+        if L==1: 
+            return min(max(ptr + ((prev_v_pwm[0]<self.table[ptr]) == (abs(prev_v_pwm[0]-output_voltage)>abs(self.table[ptr]-output_voltage))),0),255)
             
         #Search upper or lower halfs     
         if output_voltage <self.table[ptr]:
-            return search(output_voltage,ptr-ptr/2,L/2,(self.table[ptr],ptr))
+            return self.search(output_voltage,int(ptr-L/4.0),L/2,(self.table[ptr],ptr))
         else:
-            return search(output_voltage,ptr+ptr/2,L/2,(self.table[ptr],ptr))        
+            return self.search(output_voltage,int(ptr+L/4.0),L/2,(self.table[ptr],ptr))        
